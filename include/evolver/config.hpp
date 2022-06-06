aa72503a0f6902420d3fd0f2e2eb2c565738358d
roguetechh <roguedotllc@gmail.com>
2022-06-06 17:58:26 -0400

    EvolverConfig constructor

--- include/evolver/config.hpp
+++ include/evolver/config.hpp
@@ -6,4 +6,12 @@ struct EvolverConfig {
   unsigned int generation_steps;
   float mutation_rate;
   unsigned int internal_neurons;
+
+  EvolverConfig(int population_size, int genome_size, int generation_steps, float mutation_rate, unsigned int internal_neurons) {
+    this->population_size = population_size;
+    this->genome_size = genome_size;
+    this->generation_steps = generation_steps;
+    this->mutation_rate = mutation_rate;
+    this->internal_neurons = internal_neurons;
+  };
 };
